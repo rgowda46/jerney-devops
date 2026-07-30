4b3a8ddbf1e385278c69f08f7db63f6c296499d5
Rohith Gowda R M <rmrohithgowda@gmail.com>
2026-07-30 13:43:37 +0530

    fix(terraform): correct VPC subnet CIDR allocation

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,8 +20,8 @@ module "vpc" {
   cidr                 = var.vpc_cidr
 
   azs                  = local.azs
-  private_subnets     = [for k,v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
-  public_subnets      = [for k,v in local.azs : cidrsubnet(var.vpc_cidr, 4, k + 48)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
+  public_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k + 3)]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true # cost saving for dev, use one per AZ for production
